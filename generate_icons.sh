cd3c4787b2734de2a203f47db9c9940aeb58fb34
Julius Range-Lüdemann <julius.range@gmail.com>
2025-05-17 16:49:34 +0200

    Update icon for windows

--- generate_icons.sh
+++ generate_icons.sh
@@ -50,10 +50,26 @@ echo "Generating 'icon_16x16.png'"
 inkscape "icon/icon_16x16@2.svg" --export-width=16 --export-filename="icon/icon_16x16.png"
 optipng -quiet "icon/icon_16x16.png"
 
+echo "Generating 'icon_256.png'"
+inkscape "icon/icon_1024.svg" --export-width=256 --export-filename="icon/icon_256.png"
+optipng -quiet "icon/icon_256.png"
+
+echo "Generating 'icon_48.png'"
+inkscape "icon/icon_1024.svg" --export-width=48 --export-filename="icon/icon_48.png"
+optipng -quiet "icon/icon_48.png"
+
+echo "Generating 'icon_32.png'"
+inkscape "icon/icon_1024.svg" --export-width=32 --export-filename="icon/icon_32.png"
+optipng -quiet "icon/icon_32.png"
+
+echo "Generating 'icon_16.png'"
+inkscape "icon/icon_1024.svg" --export-width=16 --export-filename="icon/icon_16.png"
+optipng -quiet "icon/icon_16.png"
+
 if ! [ -x "$(command -v icotool)" ]; then
     echo "'icotool' was not found. Please install it. This is part of icoutils."
     exit 1
 fi
 
 echo "Generating 'icon.ico'"
-icotool -c -o "icon/icon.ico" "icon/icon_32x32.png" "icon/icon_256x256.png"
+icotool -c -o "icon/icon.ico" "icon/icon_16.png" "icon/icon_32.png" "icon/icon_48.png" "icon/icon_256.png"
